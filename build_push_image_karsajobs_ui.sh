6eb259f3acf1a32522c0750596f5db15b291649d
daffa1602 <142196820+daffa1602@users.noreply.github.com>
2023-11-16 12:49:27 +0700

    build_push_image_karsajobs_ui.sh

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #Membuat Docker image dari Dockerfile
-docker build -t karsajobs:latest .
+docker build -t karsajobs-ui:latest .
 
 # Mengubah nama image sesuai format Docker Hub
-docker tag karsajobs:latest rifqizuhairidaffa/karsajobs:latest
+docker tag karsajobs:latest rifqizuhairidaffa/karsajobs-ui:latest
 
 #Login to Docker Hub using the environment variable
 echo $PASSWORD_DOCKER_HUB | docker login -u rifqizuhairidaffa --password-stdin
 
 # Mengunggah image ke Docker Hub
-docker push rifqizuhairidaffa/karsajobs:latest
+docker push rifqizuhairidaffa/karsajobs-ui:latest
